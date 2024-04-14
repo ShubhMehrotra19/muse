dfeb3a4b1c4a62abb1b7eb09b1fb5755a7bba2ce
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-04-14 15:51:16 +0530

    feat: created a local json file to fetch data. bug: data not loading.

--- app/home/components/suggestion-boxes/musicListComponent.tsx
+++ app/home/components/suggestion-boxes/musicListComponent.tsx
@@ -3,17 +3,16 @@ import React from 'react'
 
 interface Props {}
 
-function MusicListComponent(props: Props) {
-    const {} = props
+function MusicListComponent({imgLink, songName, artistName}: any) {
 
     return (
         <>
         <div className='flex w-full gap-10 justify-between items-center m-1 p-1 cursor-pointer'>
             <div className='flex justify-between items-center gap-2'>
-            <Image src="https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=400" alt="profile" width={50} height={50} className='rounded-full object-cover h-14 w-14' />
+            <Image src={imgLink} alt="profile" width={50} height={50} className='rounded-full object-cover h-14 w-14' />
             <div className='flex flex-col items-start justify-start'>
-                <p className='font-semibold text-base text-slate-600'>Song Name</p>
-                <p className='font-medium text-xs text-slate-400'>Artist Name</p>
+                <p className='font-semibold text-base text-slate-600'>{songName}</p>
+                <p className='font-medium text-xs text-slate-400'>{artistName}</p>
             </div>
             </div>
             <button className='text-white bg-green-600 rounded-md hover:bg-green-700 active:scale-95 transition duration-200 ease-in-out px-3 py-1'>Explore</button>
--- app/home/components/suggestion-boxes/musicSuggestion.tsx
+++ app/home/components/suggestion-boxes/musicSuggestion.tsx
@@ -1,28 +1,49 @@
-import React from 'react';
-import MusciListComponent from './musicListComponent';
+"use client"
+import React, { useState, useEffect } from 'react';
+import MusicListComponent from './musicListComponent';
 
-interface Props {}
+interface Song {
+  songName: string;
+  artistName: string;
+  imageLink: string;
+}
 
-function FriendSuggestion(props: Props) {
-  const {} = props;
+function FriendSuggestion() {
+  const [songs, setSongs] = useState<Song[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/dummyData/songs.json');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        setSongs(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <div>
       <div className='mb-1 w-full '><p className='text-base font-semibold text-slate-600'>Explore Music...</p></div>
-    <div className='border-2 border-slate-300/70 rounded-md py-1'>
-      <div className="flex flex-col items-center">
-        <div className='flex flex-col justify-between items-center h-[200px] overflow-y-scroll scroll-smooth rounded-md px-5' style={{ scrollbarWidth: 'none'}}>
-          <MusciListComponent />
-          <MusciListComponent />
-          <MusciListComponent />
-          <MusciListComponent />
-          <MusciListComponent />
-          <MusciListComponent />
-          <MusciListComponent />
-          <MusciListComponent />
-        </div>
-      </div>
-    </div>
+            <div className='border-2 border-slate-300/70 rounded-md py-1'>
+              <div className="flex flex-col items-center">
+                <div className='flex flex-col justify-between items-center h-[200px] overflow-y-scroll scroll-smooth rounded-md px-5' style={{ scrollbarWidth: 'none'}}>
+                {songs.map((song) => (
+        <MusicListComponent
+          key={song.songName}
+          songName={song.songName}
+          artistName={song.artistName}
+          imgLink={song.imageLink}
+        />
+      ))}
+                </div>
+              </div>
+            </div>
     </div>
   );
 }
