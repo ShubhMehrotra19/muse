c05bed011096fb9c3b1a4d91e4e43168bf236fb7
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-04-16 12:44:32 +0530

    feat: profile page completed

---
+++ app/profile/components/postComponent.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface Props {}
+
+function PostComponent(props: Props) {
+    const {} = props
+
+    return (
+        <>
+        <div className='w-full h-[400px] rounded-md border-blue-600/30 border-2 pb-8 mb-8 shadow-md'>
+            <div className='w-full h-[50px] rounded-b-sm bg-blue-600/60 flex justify-between items-center px-3'>
+                <div className='flex justify-center items-center gap-2'>
+                    <Image src='https://images.pexels.com/photos/20159995/pexels-photo-20159995/free-photo-of-a-black-and-white-photo-of-a-young-girl-looking-out-a-window.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' className='h-10 w-10 rounded-full object-cover' height={40} width={40} alt='profile picture' />
+                    <div className='flex flex-col justify-start items-start'>
+                        <p className='text-sm font-medium text-white'>John Doe</p>
+                        <p className='text-xs font-medium text-slate-200'>@johndoe</p>
+                        </div> 
+                </div>
+                <p className='text-sm text-white font-semibold flex justify-center items-center'><Image className='h-[25px] w-[25px]' src='/icons/heartFinal.png' height={30} width={30} alt='' /> 360</p>
+            </div>
+            <div className='p-2'>
+            <Image src='https://images.pexels.com/photos/5877254/pexels-photo-5877254.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' height={250} width={300} alt='post' className='rounded-md h-[250px] w-full object-cover' />
+            </div>
+            <p className=' overflow-ellipsis h-[50px] text-sm font-medium px-2 pb-2'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita autem, quibusdam reprehenderit eveniet laboriosam excepturi quaerat quisquam consectetur minima molestias?
+            </p>
+        </div>
+        </>
+    )
+}
+
+export default PostComponent
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,25 +1,26 @@
 "use client"
 import Image from 'next/image'
 import React, { useState } from 'react'
+import PostComponent from './components/postComponent';
 
 interface Props {}
 
 function Page(props: Props) {
     const {} = props;
 
-    // State to store the focus state of each input
-    const [inputFocusState, setInputFocusState] = useState({
-        name: false,
-        phoneNumber: false,
-        email: false,
-        password: false
+    // State to store the focus state and input values
+    const [inputState, setInputState] = useState({
+        name: { value: '', isFocused: false },
+        phoneNumber: { value: '', isFocused: false },
+        email: { value: '', isFocused: false },
+        password: { value: '', isFocused: false }
     });
 
-    // Function to update the focus state of the specified input
-    const updateFocusState = (inputName: string, isFocused: boolean) => {
-        setInputFocusState((prevState) => ({
+    // Function to update the input state when focused or blurred
+    const updateInputState = (inputName: string, value: string, isFocused: boolean) => {
+        setInputState((prevState) => ({
             ...prevState,
-            [inputName]: isFocused
+            [inputName]: { value, isFocused }
         }));
     };
 
@@ -48,31 +49,61 @@ function Page(props: Props) {
                             <Image src='https://images.pexels.com/photos/16248641/pexels-photo-16248641/free-photo-of-sunlight-over-boy-posing-with-hen.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' height={250} width={250} alt='profile picture' className='rounded-full h-[250px] w-[250px] object-cover border-slate-400/40 border-2 hover:scale-[102%] transition-all duration-300 ease-in-out' />
                         </div>
                         <div className='flex flex-col justify-center items-start w-full relative mb-5'>
-                            <p className={`text-xs font-semibold text-slate-400 bg-white px-1 py-[2px] ${inputFocusState.name ? 'absolute left-2 -top-2' : 'absolute left-2'}`}>Name</p>
-                            <input onFocus={() => updateFocusState('name', true)} onBlur={() => updateFocusState('name', false)} className='py-2 pl-2 w-full rounded-md border-2 border-slate-500 font-medium text-base text-stone-600/90' />
+                            <p className={`text-xs font-semibold text-slate-400 bg-white px-1 py-[2px] ${inputState.name.isFocused || inputState.name.value ? 'absolute left-2 -top-2' : 'absolute left-2'}`}>Name</p>
+                            <input
+                                onFocus={() => updateInputState('name', inputState.name.value, true)}
+                                onBlur={() => updateInputState('name', inputState.name.value, false)}
+                                onChange={(e) => updateInputState('name', e.target.value, inputState.name.isFocused)}
+                                value={inputState.name.value}
+                                className='py-2 pl-2 w-full rounded-md border-2 border-slate-500 font-medium text-base text-stone-600/90'
+                            />
                         </div>
                         <div className='flex flex-col justify-center items-start w-full relative mb-5'>
-                            <p className={`text-xs font-semibold text-slate-400 bg-white px-1 py-[2px] ${inputFocusState.phoneNumber ? 'absolute left-2 -top-2' : 'absolute left-2'}`}>Phone Number</p>
-                            <input onFocus={() => updateFocusState('phoneNumber', true)} onBlur={() => updateFocusState('phoneNumber', false)} className='py-2 pl-2 w-full rounded-md border-2 border-slate-500 font-medium text-base text-stone-600/90' />
+                            <p className={`text-xs font-semibold text-slate-400 bg-white px-1 py-[2px] ${inputState.phoneNumber.isFocused || inputState.phoneNumber.value ? 'absolute left-2 -top-2' : 'absolute left-2'}`}>Phone Number</p>
+                            <input
+                                onFocus={() => updateInputState('phoneNumber', inputState.phoneNumber.value, true)}
+                                onBlur={() => updateInputState('phoneNumber', inputState.phoneNumber.value, false)}
+                                onChange={(e) => updateInputState('phoneNumber', e.target.value, inputState.phoneNumber.isFocused)}
+                                value={inputState.phoneNumber.value}
+                                className='py-2 pl-2 w-full rounded-md border-2 border-slate-500 font-medium text-base text-stone-600/90'
+                            />
                         </div>
                         <div className='flex flex-col justify-center items-start w-full relative mb-5'>
-                            <p className={`text-xs font-semibold text-slate-400 bg-white px-1 py-[2px] ${inputFocusState.email ? 'absolute left-2 -top-2' : 'absolute left-2'}`}>email</p>
-                            <input onFocus={() => updateFocusState('email', true)} onBlur={() => updateFocusState('email', false)} className='py-2 pl-2 w-full rounded-md border-2 border-slate-500 font-medium text-base text-stone-600/90' />
+                            <p className={`text-xs font-semibold text-slate-400 bg-white px-1 py-[2px] ${inputState.email.isFocused || inputState.email.value ? 'absolute left-2 -top-2' : 'absolute left-2'}`}>Email</p>
+                            <input
+                                onFocus={() => updateInputState('email', inputState.email.value, true)}
+                                onBlur={() => updateInputState('email', inputState.email.value, false)}
+                                onChange={(e) => updateInputState('email', e.target.value, inputState.email.isFocused)}
+                                value={inputState.email.value}
+                                className='py-2 pl-2 w-full rounded-md border-2 border-slate-500 font-medium text-base text-stone-600/90'
+                            />
                         </div>
                         <div className='flex flex-col justify-center items-start w-full relative mb-5'>
-                            <p className={`text-xs font-semibold text-slate-400 bg-white px-1 py-[2px] ${inputFocusState.password ? 'absolute left-2 -top-2' : 'absolute left-2'}`}>Password</p>
-                            <input onFocus={() => updateFocusState('password', true)} onBlur={() => updateFocusState('password', false)} className='py-2 pl-2 w-full rounded-md border-2 border-slate-500 font-medium text-base text-stone-600/90' />
+                            <p className={`text-xs font-semibold text-slate-400 bg-white px-1 py-[2px] ${inputState.password.isFocused || inputState.password.value ? 'absolute left-2 -top-2' : 'absolute left-2'}`}>Password</p>
+                            <input
+                                onFocus={() => updateInputState('password', inputState.password.value, true)}
+                                onBlur={() => updateInputState('password', inputState.password.value, false)}
+                                onChange={(e) => updateInputState('password', e.target.value, inputState.password.isFocused)}
+                                value={inputState.password.value}
+                                className='py-2 pl-2 w-full rounded-md border-2 border-slate-500 font-medium text-base text-stone-600/90'
+                            />
                         </div>
                         <div className='flex w-full justify-center gap-8 items-center'>
-                        <button className='px-8 py-1 rounded-md bg-blue-500/80 text-white font-medium cursor-pointer hover:scale-[102%] hover:bg-blue-600/80 active:scale-95 transition-all duration-200 ease-in-out'>Edit</button>
-                        <button className='px-5 py-1 rounded-md bg-blue-600/80 text-white font-medium cursor-pointer hover:scale-[102%] hover:bg-blue-60 active:scale-95 transition-all duration-200 ease-in-out'>Submit</button>
-                    </div>
+                            <button className='px-8 py-1 rounded-md bg-blue-500/80 text-white font-medium cursor-pointer hover:scale-[102%] hover:bg-blue-600/80 active:scale-95 transition-all duration-200 ease-in-out'>Edit</button>
+                            <button className='px-5 py-1 rounded-md bg-blue-600/80 text-white font-medium cursor-pointer hover:scale-[102%] hover:bg-blue-60 active:scale-95 transition-all duration-200 ease-in-out'>Submit</button>
+                        </div>
                     </div>
                 </div>
 
                 <div className='flex flex-col h-[84vh] px-5 py-2 w-[40%]'>
-                    <div className='border-2 border-slate-300 shadow-md rounded-md flex flex-col justify-center items-center px-4 py-1 h-full w-full'>
-                        {/* Put the post component here! */}
+                    <h2 className='text-slate-500 font-medium mb-3'>Your Posts</h2>
+                    <div className='border-2 border-slate-300 shadow-md rounded-md flex flex-col px-4 py-3 h-full w-full overflow-y-scroll' style={{
+                        scrollbarWidth: 'none'
+                    }}>
+                        <PostComponent />
+                        <PostComponent />
+                        <PostComponent />
+                        <PostComponent />
                     </div>
                 </div>
             </section>
